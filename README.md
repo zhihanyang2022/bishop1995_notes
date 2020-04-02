dc3517250fcba0a1ef30cb3a8c42cd5413c1e08e
Zhihan Yang <yangz2@carleton.edu>
2020-04-02 16:59:49 +0000

    Finished c4 jacobian.

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
-# Notes for Bishop 1995
+# My notes on Bishop 1995
 
 Remarks:
 
-- My notes and demos for understanding Neural Networks for Pattern Recognition (Bishop, 1995). 
-- This repo is not meant for sharing, but using resources here is encouraged. 
+- This repo contains my notes and demos for understanding Neural Networks for Pattern Recognition (Bishop, 1995). 
+- This repo is not meant for sharing, but resources here are well-documented and using them is more than welcomed. 
 
-Where are things located:
+Where things are located:
 
-- `painted_data_to_numpy_array.md` is stored in `./c3_single_layer_network/perceptron`.
+- `painted_data_to_numpy_array.md` is stored in `./c3_single_layer_network/perceptron`. This document shows how to convert a CSV (containing data painted and formatted in Orange https://orange.biolab.si/) into Numpy arrays. If you have not used Orange before, it contains an excellent tool called "Paint Data" for creating custom 2D data sets for testing models. Of course, it has other functionalities that I seldom use. Examples of CSVs painted in Orange:
+    - `./c3_single_layer_network/least_square_method/regression_data.csv`
+    - `./c3_single_layer_network/perceptron/data_not_linearly_separable.csv`
+    - `./c4_multi_layer_perceptron/circuluar_data.csv`
+    - `./c4_multi_layer_perceptron/data_sophisticated_decision_boundary.csv`
 
+- To learn about how to use modules (with callback and lr-finding functionalities) in `./modules_for_nn_training`, see `./c4_multi_layer_perceptron/two_layer_network_universality_demo.ipynb`.
--- modules_for_nn_training/trainer.py
+++ modules_for_nn_training/trainer.py
@@ -1,7 +1,8 @@
-import math
 import numpy as np
 from tqdm import tqdm_notebook
+import matplotlib
 import matplotlib.pyplot as plt
+import torch
 
 class Trainer:
     
@@ -9,22 +10,49 @@ class Trainer:
         self.learn = learn
         self.cb_handler = cb_handler
         
-    def find_lr(self, init_value = 1e-7, final_value=10., beta = 0.98):
+    @staticmethod
+    def plot_lrs_and_losses(lrs, losses, lr_axis_min, lr_axis_max, show_suggestion):
         
-        num = 200
-        mult = (final_value / init_value) ** (1 / num)
+        plt.plot(lrs, losses)
+        
+        if show_suggestion:  # does not work unless losses are rather smooth
+            min_grad_idx = np.argmin(np.gradient(losses))
+            plt.scatter(lrs[min_grad_idx], losses[min_grad_idx], color='red')
+        
+        plt.grid(which='major'); plt.grid(which='minor', axis='x')
+        plt.xscale('log')
+    
+        # base 10 ticks is easier to read off of
+        xticks = []
+        value = lr_axis_min
+        while value <= lr_axis_max:
+            xticks.append(value)
+            value *= 10
+            
+        plt.xticks(xticks)
+        # reference: https://github.com/fastai/fastai/blob/3b7c453cfa3845c6ffc496dd4043c07f3919270e/fastai/basic_train.py#L540
+        plt.gca().xaxis.set_major_formatter(plt.FormatStrFormatter('%.0e'))
+        
+        plt.xlabel('Learning rate'); plt.ylabel('Loss')
+        plt.show()
+        
+    def find_lr(self, init_value=1e-6, final_value=10, num_itr=200, beta=0.98, skip_start=10, skip_end=5, show_suggestion=False):
+        
+        mult = (final_value / init_value) ** (1 / num_itr)
         lr = init_value
         
         for param_group in self.learn.opt.param_groups:
             param_group['lr'] = lr
         
         avg_loss = 0.
-        best_loss = 0.
+        best_loss = np.inf
         batch_num = 0
         losses = []
         lrs = []
         
-        num_epochs = int(np.ceil(num / len(self.learn.train_dl)))
+        num_epochs = int(np.ceil(num_itr / len(self.learn.train_dl)))
+        
+        is_stop = False
         
         self.learn.model.train()
         
@@ -33,24 +61,27 @@ class Trainer:
             for xb, yb in self.learn.train_dl:
 
                 batch_num += 1
-                if batch_num > num:
-                    return lrs, losses
+                if batch_num > num_itr: 
+                    is_stop = True
+                    break
 
                 yhatb = self.learn.model(xb.float())
                 lossb = self.learn.loss(yhatb, yb.float())
 
-                #Compute the smoothed loss
+                # compute the smoothed loss
                 avg_loss = beta * avg_loss + (1-beta) * float(lossb)
-                smoothed_loss = avg_loss / (1 - beta**batch_num)
-                #Stop if the loss is exploding
-                if batch_num > 1 and smoothed_loss > 4 * best_loss:
-                    return lrs, losses
-                #Record the best loss
-                if smoothed_loss < best_loss or batch_num==1:
-                    best_loss = smoothed_loss
-                #Store the values
-                losses.append(smoothed_loss)
-                lrs.append(lr)
+                smoothed_loss = avg_loss / (1 - beta ** batch_num)
+                
+                # stop if the loss is exploding
+                if smoothed_loss > 4 * best_loss: 
+                    is_stop = True
+                    break
+                
+                # record the best loss
+                if smoothed_loss < best_loss: best_loss = smoothed_loss
+                
+                # store the values
+                losses.append(smoothed_loss); lrs.append(lr)
 
                 lossb.backward()
                 self.learn.opt.step()
@@ -59,8 +90,20 @@ class Trainer:
                 lr *= mult
                 for param_group in self.learn.opt.param_groups:
                     param_group['lr'] = lr
-            
-        return lrs, losses
+                    
+            if is_stop: break
+                
+        self.plot_lrs_and_losses(
+            lrs[skip_start:-skip_end], 
+            losses[skip_start:-skip_end],
+            init_value,
+            final_value,
+            show_suggestion
+        )
+        
+    def set_lr(self, lr):
+        for param_group in self.learn.opt.param_groups:
+            param_group['lr'] = lr
         
     def train(self, num_epoch=10):
         
@@ -104,3 +147,6 @@ class Trainer:
             self.cb_handler.on_epoch_end()
             
         self.cb_handler.on_train_end()
+        
+    def save_as_pth(self, path):
+        torch.save(self.learn.model.state_dict(), path)
