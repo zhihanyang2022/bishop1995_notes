dd323fcacaab8c3aa929bf69df9b5eb2f41d3e58
Zhihan Yang <yangz2@carleton.edu>
2020-07-03 23:47:57 +0000

    added c6 cdn using gmm with torch.distributions

--- modules_for_nn_training/utility_callbacks.py
+++ modules_for_nn_training/utility_callbacks.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from callbacks import Callback
 
-class AccuracyCallback(Callback):
+class BinaryAccuracyCallback(Callback):
     
     def on_train_begin(self):
         self.state_dict['accs'] = []
@@ -11,11 +11,45 @@ class AccuracyCallback(Callback):
         self.weighted_values = []
         self.num_examples = 0
         
-    def on_forward_end(self):
+    def eval_on_forward_end(self):
         
         preds = self.state_dict['yhatb'].detach().numpy()
         preds[preds >= 0.5] = 1
         preds[preds < 0.5] = 0
+        preds = preds.astype(int)
+        
+        targets = self.state_dict['yb'].numpy()
+        targets = targets.astype(int)
+        
+        acc = np.mean(preds == targets)
+
+        self.weighted_values.append(acc * self.state_dict['bs'])
+        self.num_examples += self.state_dict['bs']
+        
+    def on_epoch_end(self):
+        self.state_dict['accs'].append(np.sum(self.weighted_values) / self.num_examples)
+        
+    def plot(self, ax=None):
+        if ax is None:
+            ax = plt.gca()
+        ax.plot(self.state_dict['accs'])
+        ax.set_ylim(0.5, 1)
+        ax.grid()
+        ax.set_xlabel('Epoch'); ax.set_ylabel('Test acc')
+        
+class MulticlassAccuracyCallback(Callback):
+    
+    def on_train_begin(self):
+        self.state_dict['accs'] = []
+        
+    def on_epoch_begin(self):
+        self.weighted_values = []
+        self.num_examples = 0
+        
+    def eval_on_forward_end(self):
+        
+        preds = self.state_dict['yhatb'].detach().numpy()
+        preds = preds.argmax(axis=1)
         preds = preds.astype(int)
         
         targets = self.state_dict['yb'].numpy()
@@ -46,7 +80,7 @@ class LossCallback(Callback):
         self.weighted_values = []
         self.num_examples = 0
         
-    def on_loss_end(self):
+    def eval_on_loss_end(self):
         self.weighted_values.append(self.state_dict['lossb'] * self.state_dict['bs'])
         self.num_examples += self.state_dict['bs']
         
